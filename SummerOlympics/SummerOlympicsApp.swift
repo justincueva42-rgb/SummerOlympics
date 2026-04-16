1d6d064fbc0bd9d9235cfe6c7d526bd45578f693
justincueva42-rgb <justincueva42@gmail.com>
2026-04-16 10:02:04 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by CUEVA VASQUEZ, JUSTIN on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
